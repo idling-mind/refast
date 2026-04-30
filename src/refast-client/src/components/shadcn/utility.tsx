5116d3d4c84d5a8aefa0cbf5cab51623c7c94b8b
Najeem Muhammed <najeem@gmail.com>
2026-05-01 00:20:39 +0200

    scrollbar color fix

--- src/refast-client/src/components/shadcn/utility.tsx
+++ src/refast-client/src/components/shadcn/utility.tsx
@@ -102,7 +102,7 @@ export function ScrollArea({
       >
         <ScrollAreaPrimitive.Thumb
           className={cn(
-            'relative flex-1 rounded-full bg-border',
+            'relative flex-1 rounded-full bg-foreground/25 hover:bg-foreground/40 transition-colors',
             'before:absolute before:left-1/2 before:top-1/2',
             'before:h-full before:min-h-[44px] before:w-full before:min-w-[44px]',
             'before:-translate-x-1/2 before:-translate-y-1/2 before:content-[""]'
@@ -118,7 +118,7 @@ export function ScrollArea({
       >
         <ScrollAreaPrimitive.Thumb
           className={cn(
-            'relative flex-1 rounded-full bg-border',
+            'relative flex-1 rounded-full bg-foreground/25 hover:bg-foreground/40 transition-colors',
             'before:absolute before:left-1/2 before:top-1/2',
             'before:h-full before:min-h-[44px] before:w-full before:min-w-[44px]',
             'before:-translate-x-1/2 before:-translate-y-1/2 before:content-[""]'
